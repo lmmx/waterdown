a67206bc3ead0640f99e4ca3edc25a29f8c89ae5
Louis <louismmx@gmail.com>
2019-01-12 11:42:49 +0000

    Initial image IO

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # waterdown
+
 Remove image watermarks with numpy
+
+## Project goals
+
+- Use numpy to detect and reproduce the original watermark (produce output from which to load for future use)
+- Inpaint/offset the watermark region so as to unmark the image
+
+## Potential stretch goals
+
+- Calculate the alpha opacity of the watermark
+- Apply to a gif (of a different size to the still images)
+
+## Example usage (interactive)
+
+So far all I've done is to focus on the watermark region in question:
+
+```py
+img = read_image('kirby001_01.png')
+img2 = read_image('kirby001_02.png')
+wm = get_wm(img)
+wm2 = get_wm(img2)
+```
---
+++ src/waterdown.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+from imageio import imread
+
+###################### Basic image functions ##########################
+
+def read_image(img_name):
+    """
+    Read an image file (.png) into a numpy array in which each entry is
+    a row of pixels (i.e. ``len(img)`` is the image height in px.
+    """
+    data_dir = Path('..') / 'img'
+    img = imread(data_dir / img_name)
+    return img
+
+def show_image(img):
+    """
+    Show an image using a provided pixel row array.
+    """
+    plt.imshow(img)
+    plt.show()
+    return
+
+def show_original(img_name):
+    """
+    Debugging/development: produce and display an original image
+    """
+    img = read_image(img_name)
+    show_image(img)
+    return img
+
+###################### Basic image functions ##########################
+
+def get_wm(img):
+    watered = img[6:20, 13:119]
+    return watered
+
+def show_wm(img):
+    watered = get_wm(img)
+    show_image(watered)
+    return
